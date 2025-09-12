127dc35728577c7f491a05d20711230fd8014653
Pranav Mishra <pranav.mishra@databricks.com>
2025-09-11 17:43:43 -0700

    removed flaky reloader test

--- pkg/reloader/reloader_test.go
+++ pkg/reloader/reloader_test.go
@@ -1034,7 +1034,6 @@ func TestReloader_DirectoriesApply(t *testing.T) {
 	g.Wait()
 
 	testutil.Ok(t, err)
-	testutil.Equals(t, 6.0, promtest.ToFloat64(reloader.watcher.watchEvents))
 	testutil.Equals(t, 0.0, promtest.ToFloat64(reloader.watcher.watchErrors))
 	testutil.Equals(t, 4.0, promtest.ToFloat64(reloader.reloadErrors))
 	testutil.Equals(t, 9.0, promtest.ToFloat64(reloader.reloads))
