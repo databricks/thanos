2b1027ce66e7be1483f2a1625f26d103c53c7a84
Gavin Shriver <gavin.shriver@databricks.com>
2026-02-19 18:54:00 +0000

    whoops

--- pkg/syncutil/pool_test.go
+++ pkg/syncutil/pool_test.go
@@ -39,13 +39,9 @@ func TestPool_WithReset(t *testing.T) {
 	// after returning it to the pool, but this is for testing.
 	testutil.Equals(t, "cleared", buf.String())
 
-	buf2, ret := p.Get()
-	defer ret(buf2)
-	if buf2 == buf { // Note: sometimes the same object is returned, sometimes it is not.
-		testutil.Equals(t, "cleared", buf2.String())
-	} else {
-		testutil.Equals(t, "", buf2.String())
-	}
+	buf, ret = p.Get()
+	defer ret(buf)
+	testutil.Equals(t, "cleared", buf.String())
 }
 
 func TestPool_WithByteSlicePointer(t *testing.T) {
@@ -68,14 +64,11 @@ func TestPool_WithByteSlicePointer(t *testing.T) {
 	// after returning it to the pool, but this is for testing.
 	testutil.Equals(t, 24, cap(*buf))
 	testutil.Equals(t, 0, len(*buf))
-	buf2, ret := p.Get()
+	buf, ret = p.Get()
 	defer ret(buf)
-	if buf2 == buf { // Note: sometimes the same object is returned, sometimes it is not.
-		testutil.Equals(t, 24, cap(*buf2))
-	} else {
-		testutil.Equals(t, 10, cap(*buf2))
-	}
-	testutil.Equals(t, 0, len(*buf2))
+	testutil.Equals(t, 24, cap(*buf))
+	testutil.Equals(t, 0, len(*buf))
+	testutil.Equals(t, []byte{}, *buf)
 }
 
 func TestPool_Disabled(t *testing.T) {
