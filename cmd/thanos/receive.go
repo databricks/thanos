f85cf2ded8aaef29e4fe696df3c91bca83713d98
Gavin Shriver <gavin.shriver@databricks.com>
2026-02-20 19:07:07 +0000

    flags

--- cmd/thanos/receive.go
+++ cmd/thanos/receive.go
@@ -351,7 +351,8 @@ func runReceive(
 		AsyncForwardWorkerCount:  conf.asyncForwardWorkerCount,
 		ReplicationProtocol:      receive.ReplicationProtocol(conf.replicationProtocol),
 		TenantAttributor:         tenantAttributor,
-		PoolingDisabled:          !conf.poolingEnabled,
+		PoolingEnabled:           conf.poolingEnabled,
+		InitialCompressedBufCap:  conf.initialCompressedBufCap,
 		MaxPooledCompressedCap:   conf.maxPooledCompressedCap,
 		MaxPooledDecompressedCap: conf.maxPooledDecompressedCap,
 	})
@@ -1046,6 +1047,7 @@ type receiveConfig struct {
 
 	// Pool configuration for receive-path buffer reuse.
 	poolingEnabled           bool
+	initialCompressedBufCap  int
 	maxPooledCompressedCap   int
 	maxPooledDecompressedCap int
 }
@@ -1256,14 +1258,13 @@ func (rc *receiveConfig) registerFlag(cmd extkingpin.FlagClause) {
 		Default("false").BoolVar(&rc.verifyTenantAttribution)
 
 	cmd.Flag("receive.pooling-enabled", "Enable pooling of buffers for receive-path request handling.").
-		Default("false").
-		BoolVar(&rc.poolingEnabled)
+		Default("true").BoolVar(&rc.poolingEnabled)
+	cmd.Flag("receive.initial-compressed-buf-cap", "Initial capacity (bytes) allocated for compressed-request read buffers obtained from the pool.").
+		Default(fmt.Sprintf("%d", receive.DefaultInitialCompressedBufCap)).IntVar(&rc.initialCompressedBufCap)
 	cmd.Flag("receive.max-pooled-compressed-cap", "Maximum capacity (bytes) of a compressed buffer that will be returned to the pool. Buffers larger than this are discarded to prevent pool ballooning.").
-		Default(fmt.Sprintf("%d", receive.DefaultMaxPooledCompressedCap)).
-		IntVar(&rc.maxPooledCompressedCap)
+		Default(fmt.Sprintf("%d", receive.DefaultMaxPooledCompressedCap)).IntVar(&rc.maxPooledCompressedCap)
 	cmd.Flag("receive.max-pooled-decompressed-cap", "Maximum capacity (bytes) of a decompressed buffer that will be returned to the pool. Buffers larger than this are discarded to prevent pool ballooning.").
-		Default(fmt.Sprintf("%d", receive.DefaultMaxPooledDecompressedCap)).
-		IntVar(&rc.maxPooledDecompressedCap)
+		Default(fmt.Sprintf("%d", receive.DefaultMaxPooledDecompressedCap)).IntVar(&rc.maxPooledDecompressedCap)
 }
 
 // determineMode returns the ReceiverMode that this receiver is configured to run in.
--- pkg/receive/handler.go
+++ pkg/receive/handler.go
@@ -148,8 +148,8 @@ type Options struct {
 	TenantAttributor        *TenantAttributor
 
 	// Pool configuration for receive-path buffer reuse.
-	// Note: If any of the capacity options are not set, we will use defaults.
-	PoolingDisabled          bool
+	PoolingEnabled           bool
+	InitialCompressedBufCap  int
 	MaxPooledCompressedCap   int
 	MaxPooledDecompressedCap int
 }
@@ -320,14 +320,14 @@ func NewHandler(logger log.Logger, o *Options) *Handler {
 			}, []string{"tenant", "source"},
 		),
 		compressedBufPool: syncutil.NewPool(func() *bytes.Buffer {
-			return bytes.NewBuffer(make([]byte, 0, DefaultInitialCompressedBufCap))
+			return bytes.NewBuffer(make([]byte, 0, o.InitialCompressedBufCap))
 		}).WithReset(func(b *bytes.Buffer) bool {
-			if b.Cap() <= o.MaxPooledCompressedCap {
+			if b.Cap() <= cmp.Or(o.MaxPooledCompressedCap, DefaultMaxPooledCompressedCap) {
 				b.Reset()
 				return true // return buffer to the pool.
 			}
 			return false // discard the buffer that is too large.
-		}).WithDisabled(o.PoolingDisabled).
+		}).WithDisabled(!o.PoolingEnabled).
 			Build(),
 		decompressedBufPool: syncutil.NewPool(func() *[]byte {
 			// We do not need to allocate capacity to this buffer here,
@@ -336,19 +336,28 @@ func NewHandler(logger log.Logger, o *Options) *Handler {
 			b := make([]byte, 0)
 			return &b
 		}).WithReset(func(b *[]byte) bool {
-			if cap(*b) <= o.MaxPooledDecompressedCap {
+			if cap(*b) <= cmp.Or(o.MaxPooledDecompressedCap, DefaultMaxPooledDecompressedCap) {
 				*b = (*b)[:0]
 				return true // return buffer to the pool.
 			}
 			return false // discard the buffer that is too large.
-		}).WithDisabled(o.PoolingDisabled).Build(),
+		}).WithDisabled(!o.PoolingEnabled).
+			Build(),
 		writeRequestPool: syncutil.NewPool(func() *prompb.WriteRequest {
 			return &prompb.WriteRequest{}
 		}).WithReset(func(wreq *prompb.WriteRequest) bool {
+			// Keep the memory allocated for the slice, but clear the contents.
+			// If we call *prompb.WriteRequest.Reset() on the WriteRequest,
+			// it will replace the reference with a new struct, so we would
+			// effectively be pooling a pointer.
+			// Note: The drawback of this approach is that if the underlying
+			// proto changes (e.g. new fields are added), we need to update
+			// this code to clear the new fields.
 			wreq.Metadata = wreq.Metadata[:0]
 			wreq.Timeseries = wreq.Timeseries[:0]
 			return true
-		}).Build(),
+		}).WithDisabled(!o.PoolingEnabled).
+			Build(),
 	}
 
 	h.forwardRequests.WithLabelValues(labelSuccess)
